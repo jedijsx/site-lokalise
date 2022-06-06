14ec88435810bac64a82b439de4634acf8dcb718
Dmytro Puchkov <puchkov.dev@gmail.com>
2022-06-06 20:47:27 +0300

    work with hamburger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	$('.header-menu-actions-hamburger').click(function(){
 		$(this).toggleClass('open');
+		$('.header-menu-navbar').toggleClass('active')
+		$('.header-menu-actions-buttons').toggleClass('active')
 	});
 }); 
